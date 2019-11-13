fb48821b1c9c66aba4a86f93f4513fe294412dce
Zhiguang Liu <zhiguang.liu@intel.com>
2019-11-13 11:37:14 +0800

    save the test result in pickle file

--- Incrementalbuild_test.py
+++ Incrementalbuild_test.py
@@ -9,82 +9,99 @@ import os
 import subprocess
 import glob
 
+
 class Test_IncrementBuild(object):
 
-    def CopyFile(self,destpath):
-        NewBinFile=self.savepath+'\\'+destpath+'\\'
+    def CopyFile(self, destpath):
+        NewBinFile = self.savepath+'\\'+destpath+'\\'
         for affix in self.manifest.TestType['IncrementBuild']['FileNeedCompare']:
-            binlist=[]
+            binlist = []
             print('-----')
             print(affix)
             print(self.workspace)
             for BinFile in glob.glob(self.workspace+affix):
-                binlist.append((BinFile,os.path.getmtime(BinFile)))
-            binlist=sorted(binlist, key=lambda k: k[1])  
+                binlist.append((BinFile, os.path.getmtime(BinFile)))
+            binlist = sorted(binlist, key=lambda k: k[1])
             if '*' in affix:
-                index=affix.find('*')
-                name=affix[index+1:]
+                index = affix.find('*')
+                name = affix[index+1:]
             else:
                 if '\\' in affix:
-                    index=affix.find('\\')
-                    name=affix[index+1:]
-                    while index!=-1:
-                        name=name[index+1:]
-                        index=name.find('\\')
+                    index = affix.find('\\')
+                    name = affix[index+1:]
+                    while index != -1:
+                        name = name[index+1:]
+                        index = name.find('\\')
                 else:
-                    name=affix
-            if binlist!=[]:
-                shutil.copyfile(binlist[-1][0],os.path.join(NewBinFile,os.path.basename(name)))
+                    name = affix
+            if binlist != []:
+                shutil.copyfile(
+                    binlist[-1][0], os.path.join(NewBinFile, os.path.basename(name)))
             else:
                 raise NameError('no such file')
 
-    def CompareBin(self,clean,increment):
-        CleanPath=self.savepath+'\\'+clean+'\\'
-        IncrementPath=self.savepath+'\\'+increment+'\\'
-        CleanList=os.listdir(CleanPath)
-        IncrementList=os.listdir(IncrementPath)
+    def CompareBin(self, clean, increment):
+        CleanPath = self.savepath+'\\'+clean+'\\'
+        IncrementPath = self.savepath+'\\'+increment+'\\'
+        CleanList = os.listdir(CleanPath)
+        IncrementList = os.listdir(IncrementPath)
         print(CleanList)
         print(IncrementList)
-        if len(CleanList)!=len(IncrementList):
+        if len(CleanList) != len(IncrementList):
             return False
-        CompareResult=filecmp.cmpfiles(CleanPath,IncrementPath,CleanList)
-        if len(CompareResult[1])+len(CompareResult[2]) !=0:
+        CompareResult = filecmp.cmpfiles(CleanPath, IncrementPath, CleanList)
+        if len(CompareResult[1])+len(CompareResult[2]) != 0:
             return False
         return True
 
     def CleanOldFile(self):
-        BinFilePath=self.savepath+'\\'+'CleanPath'+'\\'
+        BinFilePath = self.savepath+'\\'+'CleanPath'+'\\'
         if os.path.exists(BinFilePath):
             shutil.rmtree(BinFilePath)
         os.makedirs(BinFilePath)
-        BinFilePath=self.savepath+'\\'+'IncrementPath'+'\\'
+        BinFilePath = self.savepath+'\\'+'IncrementPath'+'\\'
         if os.path.exists(BinFilePath):
             shutil.rmtree(BinFilePath)
         os.makedirs(BinFilePath)
 
     @pytest.mark.IncrementBuild
-    def test_IncrementBuild(self,PatchList):
+    def test_IncrementBuild(self, PatchList):
+
+        if PatchList[2] == 'Tested Success':
+            return
+
+        self.PatchList.ChangeStatus(PatchList,'Build Fail')
+        PatchList[2] = 'Build Fail'
 
         print('begin to test')
         self.CleanOldFile()
         self.repo_mgr.clean_all()
         self.repo_mgr.reset_all()
 
-        #Clean build
-        self.TargetRepo.checkout(PatchList[1],'clean')
+        # Clean build
+        self.TargetRepo.checkout(PatchList[1], 'clean')
         self.TargetRepo.apply_patches([PatchList[0]])
-        BuildPlatform(self.manifest.Defines.get("workspace"),self.manifest.BuildCate.get("Basic"),[],self.repo_mgr)
+        BuildPlatform(self.manifest.Defines.get("workspace"),
+                      self.manifest.BuildCate.get("Basic"), [], self.repo_mgr)
         self.CopyFile('CleanPath')
 
         self.repo_mgr.clean_all()
         self.repo_mgr.reset_all()
 
-        #Incremental build
-        self.TargetRepo.checkout(PatchList[1],'increment')
-        BuildPlatform(self.manifest.Defines.get("workspace"),self.manifest.BuildCate.get("Basic"),[],self.repo_mgr) 
+        # Incremental build
+        self.TargetRepo.checkout(PatchList[1], 'increment')
+        BuildPlatform(self.manifest.Defines.get("workspace"),
+                      self.manifest.BuildCate.get("Basic"), [], self.repo_mgr)
         self.TargetRepo.apply_patches([PatchList[0]])
-        BuildPlatform(self.manifest.Defines.get("workspace"),self.manifest.BuildCate.get("Basic"),['BuildClean'],self.repo_mgr)   
+        BuildPlatform(self.manifest.Defines.get("workspace"), self.manifest.BuildCate.get(
+            "Basic"), ['BuildClean'], self.repo_mgr)
         self.CopyFile('IncrementPath')
 
+        IsSame = self.CompareBin('CleanPath', 'IncrementPath')
 
-        assert self.CompareBin('CleanPath','IncrementPath')
+        if IsSame:
+            self.PatchList.ChangeStatus(PatchList,'Tested Success')
+        else:
+            self.PatchList.ChangeStatus(PatchList,'Tested Fail')
+
+        assert self.CompareBin('CleanPath', 'IncrementPath')
--- ManagePatchStatus.py
+++ ManagePatchStatus.py
@@ -2,61 +2,71 @@ import subprocess
 import os
 import glob
 import pickle
+
+
 class ManagePatchStatus(object):
-    def __init__(self,PicklePath=None):
-        self.PicklePath=PicklePath
+    def __init__(self, PicklePath=None):
+        self.PicklePath = PicklePath
         try:
             pkl_file = open(PicklePath, 'rb')
-            pickedata=pickle.load(pkl_file)
+            pickedata = pickle.load(pkl_file)
             pkl_file.close()
-            if pickedata[0]=='Pickle data 1.0':
-                self.StatusList=pickedata[2]
+            if pickedata[0] == 'Pickle data 1.0':
+                self.StatusList = pickedata[1]
                 print('-'*5+' patch list '+'-'*5)
                 for e in self.StatusList:
                     print(e)
                 print('-'*22)
         except:
             raise('Wrong Pickle file')
+
     @property
     def AllPatchList(self):
         return self.StatusList
-    
+
     def SaveAllPatchList(self):
         output = open(self.PicklePath, 'wb')
-        pickedata=['Pickle data 1.0',self.StatusList]
+        pickedata = ['Pickle data 1.0',self.StatusList]
         pickle.dump(pickedata, output, protocol=None)
         output.close()
 
+    def ChangeStatus(self, element, newstatus):
+        if element in self.StatusList:
+            n = self.StatusList.index(element)
+            self.StatusList[n][2] = newstatus
+            self.SaveAllPatchList()
+
+
 class GeneratePatch(ManagePatchStatus):
-    def __init__(self,AfterDate=None,Number=None,repo=r'D:\DCG\DCG10nm\Intel',PatchDir=r'D:\pytest1\patch',PicklePath=None):
+    def __init__(self, AfterDate=None, Number=None, repo=r'D:\DCG\DCG10nm\Intel', PatchDir=r'D:\pytest1\patch', PicklePath=None):
         if PatchDir:
-            self.PatchDir=PatchDir
+            self.PatchDir = PatchDir
             if not os.path.exists(self.PatchDir):
                 os.makedirs(self.PatchDir)
         else:
             raise("Please specify the directory where patches can be saved")
         if PicklePath:
-            self.PicklePath=PicklePath
+            self.PicklePath = PicklePath
         else:
-            self.PicklePath = os.path.join(self.PatchDir,'status.pickle')
-        
-        self.repo=repo
+            self.PicklePath = os.path.join(self.PatchDir, 'status.pickle')
+
+        self.repo = repo
 
         for OldPatchFile in glob.glob(os.path.join(self.PatchDir, '*.patch')):
             os.remove(OldPatchFile)
-        
+
         if os.path.exists(self.PicklePath):
             os.remove(self.PicklePath)
 
         if Number or AfterDate:
             if Number:
-                self.commit_limit=' -{0}'.format(Number)
+                self.commit_limit = ' -{0}'.format(Number)
                 if AfterDate:
                     raise("Can't assain both AfterDate and Number")
             if AfterDate:
-                self.commit_limit=' ---since='+AfterDate
+                self.commit_limit = ' ---since='+AfterDate
         else:
-            self.commit_limit=' -5'
+            self.commit_limit = ' -5'
         self.OutputPatch()
         print('-'*5+' patch list '+'-'*5)
         for e in self.AllPatchList:
@@ -64,54 +74,51 @@ class GeneratePatch(ManagePatchStatus):
         print('-'*22)
 
     def GenerateCommit(self):
-        cmd='git log --format=%H'+self.commit_limit
-        rt=subprocess.run(cmd,cwd=self.repo,text=True,capture_output=True)
+        cmd = 'git log --format=%H'+self.commit_limit
+        rt = subprocess.run(cmd, cwd=self.repo, text=True, capture_output=True)
         if rt.returncode != 0:
             print(rt.stderr)
             print(rt.stdout)
-            print('"'+cmd+'"'+ ' has error')
+            print('"'+cmd+'"' + ' has error')
             exit(1)
-        CommitList=rt.stdout.split('\n')
+        CommitList = rt.stdout.split('\n')
         CommitList.pop()
-        cmd='git log --format=%H'+' -'+str(len(CommitList)+1)
+        cmd = 'git log --format=%H'+' -'+str(len(CommitList)+1)
 
-        rt=subprocess.run(cmd,cwd=self.repo,text=True,capture_output=True)
+        rt = subprocess.run(cmd, cwd=self.repo, text=True, capture_output=True)
         if rt.returncode != 0:
             print(rt.stderr)
             print(rt.stdout)
-            print('"'+cmd+'"'+ ' has error')
+            print('"'+cmd+'"' + ' has error')
             exit(1)
 
-        CommitList=rt.stdout.split('\n')
+        CommitList = rt.stdout.split('\n')
         CommitList.pop()
         return CommitList[::-1]
-    
+
     def OutputPatch(self):
-        cmd = 'git format-patch '+self.commit_limit+ ' -o' + self.PatchDir
-        rt=subprocess.run(cmd,cwd=self.repo,text=True,capture_output=True)
+        cmd = 'git format-patch '+self.commit_limit + ' -o' + self.PatchDir
+        rt = subprocess.run(cmd, cwd=self.repo, text=True, capture_output=True)
         if rt.returncode != 0:
             print(rt.stderr)
             print(rt.stdout)
-            print('"'+cmd+'"'+ ' has error')
+            print('"'+cmd+'"' + ' has error')
             exit(1)
 
-        PatchList=rt.stdout.split('\n')
+        PatchList = rt.stdout.split('\n')
         PatchList.pop()
         if len(PatchList) != 1:
-            PatchList=PatchList[1:]
+            PatchList = PatchList[1:]
 
-        self.StatusList=[]
-        CommitList=self.GenerateCommit()
+        self.StatusList = []
+        CommitList = self.GenerateCommit()
         for i in range(len(PatchList)):
-            self.StatusList.append([PatchList[i],CommitList[i],'NotTest'])
-        output = open(self.PicklePath, 'wb')
-        pickedata=['Pickle data 1.0',self.repo,self.StatusList]
-        pickle.dump(pickedata, output, protocol=None)
-        output.close()
+            self.StatusList.append([PatchList[i], CommitList[i], 'NotTest'])
+        self.SaveAllPatchList()
         return self.StatusList
-        
-if __name__ == '__main__':
-    a=ManagePatchStatus(PicklePath=r'D:\pytest1\patch\status.pickle')
-    a.AllPatchList[0][2]='Tested_OK'
-    a.SaveAllPatchList()
 
+
+if __name__ == '__main__':
+    a = ManagePatchStatus(PicklePath=r'D:\pytest1\patch\status.pickle')
+    a.AllPatchList[0][2] = 'Tested_OK'
+    a.SaveAllPatchList()
